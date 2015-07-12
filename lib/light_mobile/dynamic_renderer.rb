af2b7b0e37c9b4508d8cad5993b39a5b76dcfebe
kaspernj <k@spernj.org>
2015-07-12 11:51:17 +0200

    Added way to force mobile version through params to easier write tests

--- lib/light_mobile/dynamic_renderer.rb
+++ lib/light_mobile/dynamic_renderer.rb
@@ -2,6 +2,12 @@ module LightMobile::DynamicRenderer
   # In order to only render mobile views if they exist.
   def render(*args, &blk)
     if request.user_agent && view_context.agent_mobile? && (session[:light_mobile_version].to_s == "mobile" || !session[:light_mobile_version].present?)
+      render_mobile = true
+    elsif params[:mobile].to_s == '1'
+      render_mobile = true
+    end
+
+    if render_mobile
       name = args[0]
       name = action_name.to_sym if !name.is_a?(Symbol) && !name.is_a?(String)
 
